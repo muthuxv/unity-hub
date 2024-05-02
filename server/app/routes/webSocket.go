cadbf503fca51e5ebbef7261f86585ad41de81f2
SimonBTSSio <simon.farnault.sio@gmail.com>
2024-05-02 12:41:37 +0000

    ws

--- server/app/routes/webSocket.go
+++ server/app/routes/webSocket.go
@@ -9,4 +9,8 @@ func WebSocketRoutes(r *gin.Engine) {
 	r.GET("/ws", func(c *gin.Context) {
         services.WsHandler(c.Writer, c.Request)
     })
+
+	r.GET("/channels/:id/send", func(c *gin.Context) {
+        services.ChannelWsHandler(c.Writer, c.Request, c.Param("id"))
+    })
 }
--- server/app/services/webSocket.go
+++ server/app/services/webSocket.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	"log"
+	"app/db/models"
+	"app/db"
+	"strconv"
 )
 
 var upgrader = websocket.Upgrader{
@@ -35,4 +38,49 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 	}
+}
+
+func ChannelWsHandler(w http.ResponseWriter, r *http.Request, channelId string) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("Upgrade error:", err)
+        return
+    }
+    defer conn.Close()
+
+    channelIDUint, err := strconv.ParseUint(channelId, 10, 32)
+    if err != nil {
+        log.Println("Channel ID conversion error:", err)
+        return
+    }
+
+    log.Printf("WebSocket connected for channel ID: %d\n", channelIDUint)
+    
+    for {
+        _, msg, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Read message error:", err)
+            break
+        }
+        
+        log.Printf("Received message on channel %d: %s\n", channelIDUint, msg)
+        
+        saveMessageToChannel(uint(channelIDUint), string(msg))
+
+        err = conn.WriteMessage(websocket.TextMessage, msg)
+        if err != nil {
+            log.Println("Write message error:", err)
+            break
+        }
+    }
+}
+
+func saveMessageToChannel(channelID uint, messageContent string) {
+    newMessage := models.Message{
+        Content:   messageContent,
+        ChannelID: channelID,
+        UserID: 1, 
+    }
+	
+	db.GetDB().Create(&newMessage)
 }
