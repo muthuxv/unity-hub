d44af5b8b001b4ba3dc9a255dd4726dc223cfeb6
Ayman-BEDDA <bedda.ayman.sio@gmail.com>
2024-07-19 12:25:31 +0200

    fix

--- server/app/routes/friend.go
+++ server/app/routes/friend.go
@@ -14,8 +14,8 @@ func FriendRoutes(r *gin.Engine) {
 	r.POST("/friends/accept", controllers.TokenAuthMiddleware("user"), services.AcceptFriend())
 	r.POST("/friends/refuse", controllers.TokenAuthMiddleware("user"), services.RefuseFriend())
 	r.GET("/friends/search/:pseudo", controllers.TokenAuthMiddleware("user"), services.SearchUser())
-	r.GET("/friends/users/:id", controllers.TokenAuthMiddleware("user"), controllers.IsOwner(), services.GetFriendsByUser())
-	r.GET("/friends/pending/:id", controllers.TokenAuthMiddleware("user"), controllers.IsOwner(), services.GetPendingFriendsByUser())
+	r.GET("/friends/users/:id", controllers.TokenAuthMiddleware("user"), services.GetFriendsByUser())
+	r.GET("/friends/pending/:id", controllers.TokenAuthMiddleware("user"), services.GetPendingFriendsByUser())
 	r.GET("/friends/sent/:id", controllers.TokenAuthMiddleware("user"), controllers.IsOwner(), services.GetPendingFriendsFromUser())
 	r.POST("/friends/request", controllers.TokenAuthMiddleware("user"), services.CreateFriendRequest())
 }
